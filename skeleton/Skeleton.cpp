b505ba94db0a805ddaf42080edb68c009760d376
Yulun Yao <allen980123@gmail.com>
2022-03-21 07:05:21 -0400

    lesson7

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -1,5 +1,8 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Constant.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
@@ -11,8 +14,31 @@ namespace {
     SkeletonPass() : FunctionPass(ID) {}
 
     virtual bool runOnFunction(Function &F) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
-      return false;
+      errs() << "Function " << F.getName()<< "\n";
+      bool changed = false;
+
+      for (auto& B : F) {
+        for (auto& I : B) {
+          if(auto* op = dyn_cast<BinaryOperator>(&I)) {
+            if(Instruction::isCommutative(I.getOpcode())){
+              IRBuilder<> builder(op);
+              Value* first = op->getOperand(0);
+              Value* second = op->getOperand(1);
+
+              if (auto* l = dyn_cast<ConstantInt>(first)) {
+                if (auto* r = dyn_cast<ConstantInt>(second)){
+                  if (l->getSExtValue() > r->getSExtValue()){
+                    op->setOperand(0, second);
+                    op->setOperand(1, first);
+                    changed = true;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      return changed;
     }
   };
 }
